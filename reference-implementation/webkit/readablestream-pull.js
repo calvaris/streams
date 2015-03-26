91f76c011146b7c2ea34e76ab9cfe7b6a650def9
Xabier Rodriguez Calvar <calvaris@igalia.com>
2015-03-26 18:22:09 +0100

    Fixed ReadableStream pull should be able to close a stream

--- reference-implementation/webkit/readablestream-pull.js
+++ reference-implementation/webkit/readablestream-pull.js
@@ -4,12 +4,12 @@ var test1 = async_test('ReadableStream pull should be able to close a stream.');
 test1.step(function() {
     var rs = new ReadableStream({
         pull: function(enqueue, close, error) {
-            close()
+            close();
         }
     });
 
-    rs.ready.then(test1.step_func(function() {
-        assert_equals(rs.state, 'closed');
+    var reader = rs.getReader();
+    reader.closed.then(test1.step_func(function() {
         test1.done();
     }));
 });
